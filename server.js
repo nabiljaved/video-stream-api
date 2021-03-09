71c30b4b8e28bd368d45bdf0c29de64ff82e8643
nabiljaved <nabeeljaved2029@gmail.com>
2021-03-09 18:12:12 +0500

    show stream dynamic videos

--- server.js
+++ server.js
@@ -8,41 +8,52 @@ app.get('/', function (req, res)
     res.sendFile(path.join(__dirname + '/index.html'))
 })
 
-app.get('/video', function (req, res)
+app.get('/video/:videoname', function (req, res)
 {
-    const path = 'assets/vermedin.mp4'
-    const stat = fs.statSync(path)
-    const fileSize = stat.size
-    const range = req.headers.range
+    console.log(req.params.videoname);
 
-    if (range)
+    if (req.params.videoname == null)
     {
-        const parts = range.replace(/bytes=/, "").split("-")
-        const start = parseInt(parts[0], 10)
-        const end = parts[1]
-            ? parseInt(parts[1], 10)
-            : fileSize - 1
-
-        const chunksize = (end - start) + 1
-        const file = fs.createReadStream(path, { start, end })
-        const head = {
-            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-            'Accept-Ranges': 'bytes',
-            'Content-Length': chunksize,
-            'Content-Type': 'video/mp4',
-        }
-
-        res.writeHead(206, head)
-        file.pipe(res)
+        res.send("please mention video name")
+        return;
     } else
     {
-        const head = {
-            'Content-Length': fileSize,
-            'Content-Type': 'video/mp4',
+        const path = `assets/${req.params.videoname}.mp4`;
+        //const path = 'assets/burger.mp4'
+        const stat = fs.statSync(path)
+        const fileSize = stat.size
+        const range = req.headers.range
+
+        if (range)
+        {
+            const parts = range.replace(/bytes=/, "").split("-")
+            const start = parseInt(parts[0], 10)
+            const end = parts[1]
+                ? parseInt(parts[1], 10)
+                : fileSize - 1
+
+            const chunksize = (end - start) + 1
+            const file = fs.createReadStream(path, { start, end })
+            const head = {
+                'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+                'Accept-Ranges': 'bytes',
+                'Content-Length': chunksize,
+                'Content-Type': 'video/mp4',
+            }
+
+            res.writeHead(206, head)
+            file.pipe(res)
+        } else
+        {
+            const head = {
+                'Content-Length': fileSize,
+                'Content-Type': 'video/mp4',
+            }
+            res.writeHead(200, head)
+            fs.createReadStream(path).pipe(res)
         }
-        res.writeHead(200, head)
-        fs.createReadStream(path).pipe(res)
     }
+
 })
 
 const PORT = process.env.PORT || 5000
