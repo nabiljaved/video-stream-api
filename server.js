4469f8bcbc9a5f1c071e018a8a7184053208145d
nabiljaved <nabeeljaved2029@gmail.com>
2021-03-09 17:08:14 +0500

    play low resolution vid

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res)
 
 app.get('/video', function (req, res)
 {
-    const path = 'assets/steak.mp4'
+    const path = 'assets/vermedin.mp4'
     const stat = fs.statSync(path)
     const fileSize = stat.size
     const range = req.headers.range
